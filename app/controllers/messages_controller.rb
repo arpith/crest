35fea207fe5f6119aecc447ca0220afba7a58509
Arpith Siromoney <arpith@feedreader.co>
2016-08-19 15:27:36 -0400

    controller to list messages for pair of users

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,9 @@
 class MessagesController < ApplicationController
+	def index
+    clause = "(from = ? AND to = ?) OR (from = ? AND to = ?)"
+    @messages = Message.where(clause, params[:users], params[:users])
+	end
+ 
  def create
     @message = Message.new(params[:message])
     @message.save
