5b51df4013160c3c3e7daf49f3397e6e8cebb602
Arpith Siromoney <arpith@feedreader.co>
2016-08-19 16:01:07 -0400

    use placeholder conditions

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,7 @@
 class MessagesController < ApplicationController
 	def index
-    clause = "(from = ? AND to = ?) OR (from = ? AND to = ?)"
-    @messages = Message.where(clause, params[:users], params[:users])
+    @messages = Message.where("(from = :u1 AND to = :u2) OR (from = :u2 AND to = :u1)", 
+                              {u1: params[:users][0], u2: params[:users][1]})
 	end
  
  def create
