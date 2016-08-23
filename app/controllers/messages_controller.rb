953062459d3bebe2790bcdc057fbabf6186c1027
Arpith Siromoney <arpith@feedreader.co>
2016-08-23 16:51:50 -0400

    don't need find_in_batches

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
 	def index
     puts params[:users]
     @messages = Message.where("(sender = :u1 AND receiver = :u2) OR (sender = :u2 AND receiver = :u1)",
-                              {u1: params[:user1], u2: params[:user2]}).order(updated_at: :desc).find_in_batches
+                              {u1: params[:user1], u2: params[:user2]}).order(updated_at: :desc)
     render json: @messages
 	end
  
