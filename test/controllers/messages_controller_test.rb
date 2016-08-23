0917de44fb2eb9fa2d6be0e88e2975fd307514a2
Arpith Siromoney <arpith@feedreader.co>
2016-08-23 18:15:17 -0400

    controller tests

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class MessagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @message = messages(:one)
+    puts @message
+  end
+
+  test "should get index" do
+    get messages_url, as: :json
+    assert_response :success
+  end
+
+  test "should create message" do
+    assert_difference('Message.count') do
+      post messages_url, params: { sender: "1", receiver: "2", text: "hi" }, as: :json
+    end
+
+    assert_response 201
+  end
+
 end
