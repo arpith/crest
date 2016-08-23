a7a1c8551530acd79baee73a1e748f5d85c4f428
Arpith Siromoney <arpith@feedreader.co>
2016-08-23 17:27:14 -0400

    use assert_includes

--- test/integration/chat_flow_test.rb
+++ test/integration/chat_flow_test.rb
@@ -15,15 +15,12 @@ class ChatFlowTest < ActionDispatch::IntegrationTest
   test "can list message" do
     message = {"sender": "u1", "receiver": "u2", "text": "hi"}
     post '/messages', params: message, as: :json
-    id = JSON.parse(response.body)['id']
+    message = JSON.parse(response.body)['message']
     get '/messages', params: {user1: 'u1', user2: 'u2'}
     assert_response :success
     json = JSON.parse(response.body)
-    i = json.index { |m| m['id'] == id }
-    resp_message = json[i]
-    assert_equal message[:sender], resp_message['sender']
-    assert_equal message[:receiver], resp_message['receiver']
-    assert_equal message[:text], resp_message['text']
+    messages = json['messages']
+    assert_includes(messages, message)
   end
 
 end
